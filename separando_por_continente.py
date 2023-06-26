57a6b999a2c9ec034ed6af21cab7bb3ada6b5366
Kauan Mariani Ferreira <kauan.mariani@gmail.com>
2023-06-26 12:03:03 -0300

    Colocando a coluna continente na base de dados

---
+++ separando_por_continente.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import os
+
+# Lendo a nossa base de dados
+causas_de_morte = pd.read_csv("cause_of_deaths.csv")
+
+# Colocando o csv com os países e continentes na base de dados
+paises_e_continentes = pd.read_csv("Countries-Continents.csv")
+
+# Colocando as duas colunas como um dicionário
+dicio_paises_e_cont = dict(zip(paises_e_continentes["Country"], paises_e_continentes["Continent"]))
+
+# Colocando os continentes na base de dados
+causas_de_morte["Continente"] = causas_de_morte["Country/Territory"].map(dicio_paises_e_cont)
+
+# Colocando a base num csv
+causas_de_morte.to_csv("new_cause_of_death.csv")
+
+
+
+
+
+
+
+
+
