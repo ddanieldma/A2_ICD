70c5b3e0a9237353df61417c10f265eb1b42fb12
Kauan Mariani Ferreira <kauan.mariani@gmail.com>
2023-06-26 12:13:30 -0300

    Separarando os arquivos no qual adicionamos o continente para melhor organizar o código

--- transformando_dados/separando_por_continente.py
+++ transformando_dados/separando_por_continente.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import os
+os.chdir("transformando_dados")
 
 # Lendo a nossa base de dados
 causas_de_morte = pd.read_csv("cause_of_deaths.csv")
@@ -14,7 +15,7 @@ dicio_paises_e_cont = dict(zip(paises_e_continentes["Country"], paises_e_contine
 causas_de_morte["Continente"] = causas_de_morte["Country/Territory"].map(dicio_paises_e_cont)
 
 # Colocando a base num csv
-causas_de_morte.to_csv("new_cause_of_death.csv")
+causas_de_morte.to_csv("../new_cause_of_death.csv")
 
 
 
