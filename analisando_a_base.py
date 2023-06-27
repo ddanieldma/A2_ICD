3130be9128a20534d9e5303b287870c5c81046b6
Kauan Mariani Ferreira <kauan.mariani@gmail.com>
2023-06-27 09:25:43 -0300

    primeiro teste

--- analisando_a_base.py
+++ analisando_a_base.py
@@ -5,9 +5,36 @@ from bokeh.layouts import gridplot
 from bokeh.models.annotations import Span, BoxAnnotation
 from bokeh.models import ColumnDataSource 
 
-output_file("visualização.html")
+# Criando o arquivo de saída
+output_file("visualizacao.html")
 
+# Lendo os dados
 df_causas_de_morte = pd.read_csv("new_cause_of_death.csv")
 
-print(df_causas_de_morte)
+# Filtrando os dados para os dados que iremos analisar
+df_causas_de_morte_Brasil = df_causas_de_morte[(df_causas_de_morte["Country Code"] == "BRA")]
+
+# Definindo as variáveis
+x = df_causas_de_morte_Brasil["Year"]
+y = df_causas_de_morte_Brasil["população"]
+
+# criando o gráfico
+grafico_teste = figure()
+
+# adicionando o glifo a ele
+grafico_teste.line(x,y)
+
+# abrindo o html com o gráfico
+show(grafico_teste)
+
+
+
+# x = df_causas_de_morte["Meningitis"]
+# y = df_causas_de_morte["Year"]
+
+
+# print(df_causas_de_morte.columns)
+
+
+
 
