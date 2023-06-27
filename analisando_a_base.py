60aa9dbc4a6c500f846c6c9c50c2bda2baa209d6
Kauan Mariani Ferreira <kauan.mariani@gmail.com>
2023-06-26 22:35:53 -0300

    alguma modificação

--- analisando_a_base.py
+++ analisando_a_base.py
@@ -4,3 +4,10 @@ from bokeh.io import output_file, save, show
 from bokeh.layouts import gridplot
 from bokeh.models.annotations import Span, BoxAnnotation
 from bokeh.models import ColumnDataSource 
+
+output_file("visualização.html")
+
+df_causas_de_morte = pd.read_csv("new_cause_of_death.csv")
+
+print(df_causas_de_morte)
+
