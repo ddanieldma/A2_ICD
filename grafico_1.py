f4290b78c8de0da07949a04c85785aa3778a6814
Kauan Mariani Ferreira <kauan.mariani@gmail.com>
2023-06-28 14:56:34 -0300

    começando a fazer o grid

--- grafico_1.py
+++ grafico_1.py
@@ -25,16 +25,32 @@ top_3_values_per_continent = top_3_values_per_continent.to_frame()
 # Tirando o index como algo de multicategoria
 top_3_values_per_continent = top_3_values_per_continent.reset_index()
 
-# Fazendo um Grid para mostrar as principais causas de mortes desde 1990 no numdo
+top_3_africa = ColumnDataSource(top_3_values_per_continent.loc[top_3_values_per_continent["Continente"]=="Africa"])
+top_3_asia = ColumnDataSource(top_3_values_per_continent.loc[top_3_values_per_continent["Continente"]=="Asia"])
+top_3_europe = ColumnDataSource(top_3_values_per_continent.loc[top_3_values_per_continent["Continente"]=="Europe"])
+top_3_north_america = ColumnDataSource(top_3_values_per_continent.loc[top_3_values_per_continent["Continente"]=="North America"])
+top_3_south_america = ColumnDataSource(top_3_values_per_continent.loc[top_3_values_per_continent["Continente"]=="South America"])
+top_3_oceania = ColumnDataSource(top_3_values_per_continent.loc[top_3_values_per_continent["Continente"]=="Oceania"])
 
-grafico_africa = figure()
-grafico_asia = figure()
-grafico_europe = figure()
-grafico_north_america = figure()
-grafico_south_america = figure()
-gráfico_oceania = figure()
+# Fazendo um Grid para mostrar as principais causas de mortes desde 1990 no numdo
+grafico_africa = figure(x_range = top_3_africa.data["Name of the disease"])
+grafico_asia = figure(x_range = top_3_asia.data["Name of the disease"])
+grafico_europe = figure(x_range = top_3_europe.data["Name of the disease"])
+grafico_north_america = figure(x_range = top_3_north_america.data["Name of the disease"])
+grafico_south_america = figure(x_range = top_3_south_america.data["Name of the disease"])
+gráfico_oceania = figure(x_range = top_3_oceania.data["Name of the disease"])
+
 
 # Definindo os parâmetros para o gráfico da áfrica
+grafico_africa.vbar(x = "Name of the disease", top = "Number of cases", width = 0.8 ,source = top_3_africa)
 
-grafico_africa.width = 1020
-grafico_africa.length = 960
+# Colocando em resolução HD
+grafico_africa.width = 1280
+grafico_africa.height = 720
+
+# Definindo as propiedades do título
+grafico_africa.title.text = "Africa"
+grafico_africa.title.text_color = "Black"
+
+#mostrando o gráfico
+show(grafico_africa)
