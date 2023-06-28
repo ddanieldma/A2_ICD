ff9c6a1662bcadaaaa2bdccafae2e8c9693aa6c7
ddanieldma <danielalmemiranda@gmail.com>
2023-06-28 17:30:16 -0300

    vesrsão quase final do plot sobre homicidios

--- plots_homicidios/bar_plot_homicides.py
+++ plots_homicidios/bar_plot_homicides.py
@@ -1,7 +1,10 @@
-from organizing_data_homicides import data_homicides_by_continent, df_homicides_by_continent
+from organizing_data_homicides import data_homicides_by_continent, data_homicides_by_continent_proportion, df_homicides_by_continent
 from bokeh.plotting import figure, output_file, show
 from bokeh.layouts import gridplot
 from bokeh.models import NumeralTickFormatter
+
+import os
+os.chdir("plots_homicidios")
 
 output_file("deaths_by_homicides.html")
 
@@ -22,12 +25,16 @@ plot_interpersonal_violence.background_fill_alpha = 0.9
 
 # Cor dos eixos
 plot_interpersonal_violence.xaxis.minor_tick_line_color = "#000000"
+# removendo ticks menores do eixo y
+plot_interpersonal_violence.yaxis.minor_tick_out = 0
 
 # Cor do grid
 plot_interpersonal_violence.xgrid.grid_line_color = "#666666"
 plot_interpersonal_violence.ygrid.grid_line_color = "#666666"
 
+plot_interpersonal_violence.xaxis.major_label_orientation = 0.785
 
+#=================================
 
 # mortes por violencia interpessoal por continente
 plot_conflict_terrorism = figure(x_range = df_homicides_by_continent["Continente"].values)
@@ -46,11 +53,83 @@ plot_conflict_terrorism.background_fill_alpha = 0.9
 
 # Cor dos eixos
 plot_conflict_terrorism.xaxis.minor_tick_line_color = "#000000"
+# removendo ticks menores do eixo y
+plot_conflict_terrorism.yaxis.minor_tick_out = 0
 
 # Cor do grid
 plot_conflict_terrorism.xgrid.grid_line_color = "#666666"
 plot_conflict_terrorism.ygrid.grid_line_color = "#666666"
 
-plot_homicides_continents = gridplot([[plot_interpersonal_violence, plot_conflict_terrorism]])
+plot_conflict_terrorism.xaxis.major_label_orientation = 0.785
+
+#================================================================================================
+
+# plots com números em proporção
+
+# mortes por violencia interpessoal por continente em proporção à população total
+plot_interpersonal_violence_proportion = figure(x_range = df_homicides_by_continent["Continente"].values)
+
+# Título e dimensões do gráfico
+plot_interpersonal_violence_proportion.title = "Mortes por violencia interpessoal por continente \n em relação à população total"
+plot_interpersonal_violence_proportion.width = 400
+plot_interpersonal_violence_proportion.height = 400
+
+plot_interpersonal_violence_proportion.vbar(x = "Continente", top = "Inverted Violence Proportion", fill_color = "Red", width = 0.85, source = data_homicides_by_continent_proportion, line_color = None)
+plot_interpersonal_violence_proportion.yaxis.formatter = NumeralTickFormatter(format = '0.0a')
+
+# Bakcground do gráfico
+plot_interpersonal_violence_proportion.background_fill_color = "#242424"
+plot_interpersonal_violence_proportion.background_fill_alpha = 0.9
+
+# Cor dos ticks dos eixos
+plot_interpersonal_violence_proportion.xaxis.minor_tick_line_color = "#000000"
+# removendo ticks do eixo y
+plot_interpersonal_violence_proportion.yaxis.minor_tick_out = 0
+plot_interpersonal_violence_proportion.yaxis.major_tick_out = 0
+
+# Cor do grid
+plot_interpersonal_violence_proportion.xgrid.grid_line_color = "#666666"
+plot_interpersonal_violence_proportion.ygrid.grid_line_color = "#666666"
+
+# Rótulo dos eixos
+plot_interpersonal_violence_proportion.yaxis.major_label_text_font_size = "0pt"
+plot_interpersonal_violence_proportion.xaxis.major_label_orientation = 0.785
+
+#=================================
+
+# mortes por conflitos e terrorismo por continente em proporção à população total
+plot_conflict_terrorism_proportion = figure(x_range = df_homicides_by_continent["Continente"].values)
+
+# Título e dimensões do gráfico
+plot_conflict_terrorism_proportion.title = "Mortes por conflitos e terrorismo por continente \n em relação à população total"
+plot_conflict_terrorism_proportion.width = 400
+plot_conflict_terrorism_proportion.height = 400
+
+plot_conflict_terrorism_proportion.vbar(x = "Continente", top = "Inverted Conflict Proportion", fill_color = "Red", width = 0.85, source = data_homicides_by_continent_proportion, line_color = None)
+plot_conflict_terrorism_proportion.yaxis.formatter = NumeralTickFormatter(format = '0.0a')
+
+# Bakcground do gráfico
+plot_conflict_terrorism_proportion.background_fill_color = "#242424"
+plot_conflict_terrorism_proportion.background_fill_alpha = 0.9
+
+# Cor dos ticks dos eixos
+plot_conflict_terrorism_proportion.xaxis.minor_tick_line_color = "#000000"
+
+# removendo ticks do eixo y
+plot_conflict_terrorism_proportion.yaxis.minor_tick_out = 0
+plot_conflict_terrorism_proportion.yaxis.major_tick_out = 0
+
+# Cor do grid
+plot_conflict_terrorism_proportion.xgrid.grid_line_color = "#666666"
+plot_conflict_terrorism_proportion.ygrid.grid_line_color = "#666666"
+
+# Rótulo dos eixos
+plot_conflict_terrorism_proportion.yaxis.major_label_text_font_size = "0pt"
+plot_conflict_terrorism_proportion.xaxis.major_label_orientation = 0.785
+
+#================================================================================================
+
+# grid plot
+plot_homicides_continents = gridplot([[plot_interpersonal_violence, plot_conflict_terrorism], [plot_interpersonal_violence_proportion, plot_conflict_terrorism_proportion]])
 
 show(plot_homicides_continents)
--- plots_homicidios/organizing_data_homicides.py
+++ plots_homicidios/organizing_data_homicides.py
@@ -1,10 +1,10 @@
 import pandas as pd
 from bokeh.models import ColumnDataSource
 #================================================================================================
-
 # lendo dados
 df_cause_of_death = pd.read_csv("new_cause_of_death.csv")
 
+#================================================================================================
 # mortes provocadas por homicidios (violencia interpesoal e conflitos/terrorismo) por continentes e por ano
 df_homicides_by_continent = df_cause_of_death.groupby("Continente")[["Interpersonal Violence", "Conflict and Terrorism"]].sum().reset_index()
 
@@ -17,8 +17,24 @@ for each_continent in df_homicides_by_continent["Continente"]:
     colors.append(color_dict[each_continent])
 
 df_homicides_by_continent["Color"] = colors
+
+# colocando colunas com valores negativos para que o gráfico seja plotado de cima para baixo
 df_homicides_by_continent["Inverted Interpersonal Violence"] = df_homicides_by_continent["Interpersonal Violence"] * -1
 df_homicides_by_continent["Inverted Conflict and Terrorism"] = df_homicides_by_continent["Conflict and Terrorism"] * -1
 
 # column data source com as colunas que queremos
 data_homicides_by_continent = ColumnDataSource(df_homicides_by_continent)
+
+#================================================================================================
+# mesmos dados mas agora com o número de mortes em relação à população do respectivo continente
+
+df_homicides_by_continent_proportion = df_cause_of_death.groupby("Continente")[["Interpersonal Violence", "Conflict and Terrorism", "população"]].sum().sort_values(by = "população").reset_index()
+
+df_homicides_by_continent_proportion["Proportion Violence"] = (df_homicides_by_continent_proportion["Interpersonal Violence"] / df_homicides_by_continent_proportion["população"])
+df_homicides_by_continent_proportion["Proportion Conflict"] = (df_homicides_by_continent_proportion["Conflict and Terrorism"] / df_homicides_by_continent_proportion["população"])
+
+# colocando novamente valores negativos
+df_homicides_by_continent_proportion["Inverted Violence Proportion"] = df_homicides_by_continent_proportion["Proportion Violence"] * -1
+df_homicides_by_continent_proportion["Inverted Conflict Proportion"] = df_homicides_by_continent_proportion["Proportion Conflict"] * -1
+
+data_homicides_by_continent_proportion = ColumnDataSource(df_homicides_by_continent_proportion)
