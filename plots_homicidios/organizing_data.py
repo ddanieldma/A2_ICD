b48fb660845dde45fa01f63c37f3554a95fc636b
ddanieldma <danielalmemiranda@gmail.com>
2023-06-28 12:39:01 -0300

    fazendo ridge plot

--- plots_homicidios/organizing_data.py
+++ plots_homicidios/organizing_data.py
@@ -6,22 +6,17 @@ from bokeh.models import ColumnDataSource
 df_cause_of_death = pd.read_csv("new_cause_of_death.csv")
 
 # mortes provocadas por homicidios (violencia interpesoal e conflitos/terrorismo) por continentes e por ano
-df_homicides_by_continent = df_cause_of_death[["Interpersonal Violence", "Conflict and Terrorism", "Continente"]].groupby(["Continente"]).sum()
+df_homicides_by_continent = df_cause_of_death.groupby("Continente")[["Interpersonal Violence", "Conflict and Terrorism"]].sum().reset_index()
 
 # dicionario de cores para os continentes
 color_dict = {"Africa" : "Yellow", "North America" : "Red", "South America" : "DarkRed", "Europe" : "Black", "Oceania" : "Blue", "Asia" : "Green"}
 
 # colocando a cor para cada continente no dataframe
 colors = []
-for each_continent in df_homicides_by_continent.index:
+for each_continent in df_homicides_by_continent["Continente"]:
     colors.append(color_dict[each_continent])
 
 df_homicides_by_continent["Color"] = colors
 
 # column data source com as colunas que queremos
 data_homicides_by_continent = ColumnDataSource(df_homicides_by_continent)
-
-# o bokeh não acessa índices como se fossem colunas em column data source
-# por isso, precisamos acessar os índices do dataframe separadamente
-# e colocar em uma variável
-index_values = df_homicides_by_continent.index.values
