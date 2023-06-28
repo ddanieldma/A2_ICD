ca05350c110f300c58fa37a8cd870031a395d3ba
guilierm <cguilherme182@gmail.com>
2023-06-28 10:31:16 -0300

    mudança ideia plot 2 4/4

--- plot_hiv_africa/plot_grid_4/per_country.py
+++ plot_hiv_africa/plot_grid_4/per_country.py
@@ -6,12 +6,14 @@ from bokeh.transform import factor_cmap
 
 df = pd.read_csv("../../new_cause_of_death.csv")
 
-df_countries_hiv = df[df['Continente'] == "Africa"].groupby('Country/Territory')['HIV/AIDS'].sum().reset_index()
+df["per_100k_hab"] = df["HIV/AIDS"]/df["população"]*100000
+
+df_countries_hiv = df[df['Continente'] == "Africa"].groupby('Country/Territory')['per_100k_hab'].sum().sort_values(ascending=False).head(10).reset_index()
 
 cds = ColumnDataSource(df_countries_hiv)
 
 fig = figure(x_range=df_countries_hiv['Country/Territory'])
 
-fig.vbar(x='Country/Territory', top='HIV/AIDS', source=cds)
+fig.vbar(x='Country/Territory', top='per_100k_hab', source=cds, width = 0.8, fill_color = '#bc634f', line_color = None)
 
 show(fig)
