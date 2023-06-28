de6aa1cf4030aaa77555b67e98b6e8448319c710
Kauan Mariani Ferreira <kauan.mariani@gmail.com>
2023-06-28 17:12:55 -0300

    arrumei o gráfico de grid

---
+++ gráfico_Brasil/grafico_2.py
@@ -0,0 +1,10 @@
+import sys
+sys.path.append('../A2_ICD')
+from analisando_a_base import df_causas_de_morte
+import pandas as pd
+from bokeh.plotting import figure
+from bokeh.io import output_file, save, show
+from bokeh.layouts import gridplot
+from bokeh.models.annotations import Span, BoxAnnotation
+from bokeh.models import ColumnDataSource 
+
